5d97345100bdae0e1d96a8dbd4e87e619f3cef3d
champdecay <dhruvangg@gmail.com>
2022-02-11 22:41:55 +0530

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                     <NavLink className={({ isActive }) => isActive ? 'mr-5 text-black' : 'mr-5 text-gray-500'} to="/">Home</NavLink>
-                    <NavLink className={({ isActive }) => isActive ? 'mr-5 text-black' : 'mr-5 text-gray-500'} to="/blog">Blog</NavLink>
-                    <NavLink className={({ isActive }) => isActive ? 'mr-5 text-black' : 'mr-5 text-gray-500'} to="https://drive.google.com/file/d/1JqX1lxDO6vEObWCgiaFd_1hvimfJvRkt/view">Resume</NavLink>
+                    {/* <NavLink className={({ isActive }) => isActive ? 'mr-5 text-black' : 'mr-5 text-gray-500'} to="/blog">Blog</NavLink> */}
+                    <a className='mr-5 text-gray-500' href="https://drive.google.com/file/d/1JqX1lxDO6vEObWCgiaFd_1hvimfJvRkt/view" target="_blank">Resume</a>
                 </nav>
             </div>
         </header>
