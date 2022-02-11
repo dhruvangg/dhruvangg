ba4570ee8cec57bcb35b7ed0a3162d346241a37e
champdecay <dhruvangg@gmail.com>
2022-02-11 22:52:16 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Home from "./pages/Home"
 
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </main>
     </BrowserRouter>
   )
